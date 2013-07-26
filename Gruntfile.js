6764bbeac43ec5a9bf417c8c7dc6564d5310a4a0
Pedro Del Gallego <pedro.delgallego@gmail.com>
2013-07-26 17:50:15 +0100

    feat(build) initial implementation of the define function

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,15 @@
 /*global module:false*/
 module.exports = function(grunt) {
-
-  // Project configuration.
   grunt.initConfig({
-    // Task configuration.
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
+    mochaTest: {
+      test: {
+        options: { reporter: 'spec', growl: true },
+        src: ['test/**/*.js']
       }
     },
-    nodeunit: {
-      files: ['test/**/*_test.js']
+    jshint: {
+      gruntfile: { src: 'Gruntfile.js' },
+      lib_test:  { src: ['lib/**/*.js', 'test/**/*.js'] }
     },
     watch: {
       gruntfile: {
@@ -36,17 +18,18 @@ module.exports = function(grunt) {
       },
       lib_test: {
         files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'nodeunit']
+        tasks: ['jshint:lib_test', "mochaTest"]
       }
     }
   });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+  require('matchdep').
+    filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+
+  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('test', ['mochaTest']);
+
 
 };
---
+++ test/malyshev-spec.js
@@ -0,0 +1,103 @@
+(function(){
+  "use strict";
+  var expect  = require('chai').expect,
+      sut = require('../lib/malyshev.js');
+
+  it("should be a function", function(){expect(typeof sut.define).to.equal("function");});
+  it("should be a object",   function(){ expect(typeof sut.factories).to.equal("object");});
+  it("should be a function", function(){expect(typeof sut.build).to.equal("function");});
+
+  describe('Factory', function(){
+    describe('#define', function(){
+
+      describe("pass a simple object", function(){
+        beforeEach(function(){
+          sut.define("user", {name:"pedro"});
+        });
+
+        it("should be a create a new factory object in .factories", function(){
+          expect(typeof sut.factories.user).to.equal("object");
+        });
+
+        it("should preserve the status in the factory", function(){
+          expect(sut.factories.user.name).to.equal("pedro");
+        });
+      });
+
+      describe("pass array of statues", function(){
+        var states = [{name:"pedro"}, {surname: "Del Gallego"}];
+        beforeEach(function(){
+          sut.define("user", states);
+        });
+
+        it("should be a create a new factory object in .factories", function(){
+          expect(typeof sut.factories.user).to.equal("object");
+        });
+
+        it("should preserve the status in the factory", function(){
+          expect(sut.factories.user.name).to.equal("pedro");
+          expect(sut.factories.user.surname).to.equal("Del Gallego");
+        });
+      });
+
+      describe("pass array of statues", function(){
+        it("should be a preserve the status if the function is on the behind an object", function(){
+          var states = [function(){return {type: "user"};}, {name:"pedro"} ];
+          sut.define("user", states);
+          expect(sut.factories.user.name).to.equal("pedro");
+          expect(sut.factories.user.type).to.equal("user");
+        });
+
+        it("should preserve the status if the function is on the behind an object", function(){
+          var states = [{name:"pedro"}, function(){return {type: "user"};}];
+          sut.define("user", states);
+          expect(sut.factories.user.type).to.equal("user");
+          expect(sut.factories.user.name).to.equal("pedro");
+        });
+
+        it("should preserve the status when a constructor is used", function(){
+          var User = function(){ this.type = "user";};
+          var states = [{name:"pedro"}, new User()];
+          sut.define("user", states);
+          expect(sut.factories.user.type).to.equal("user");
+          expect(sut.factories.user.name).to.equal("pedro");
+        });
+      });
+
+      describe("pass array of factory references", function(){
+        var user   = sut.define("user", {name: "pedro"});
+        var states = sut.define("confirmedUser", {confirmed: true});
+
+        beforeEach(function(){
+          sut.define("validUser", ["user", "confirmedUser"]);
+        });
+
+        it("should be a preserve the status if the function is on the behind an object", function(){
+          expect(sut.factories.validUser.name).to.equal("pedro");
+          expect(sut.factories.validUser.confirmed).to.equal(true);
+        });
+      });
+    });
+
+    describe('.factories', function(){
+
+    });
+
+    describe('#build', function(){
+
+    });
+
+    xdescribe('#attributesFor', function(){
+      it("should be a function", function(){
+        expect(typeof sut.attributesFor).to.equal("function");
+      });
+    });
+
+    xdescribe('#sequence', function(){
+      it("should be a function", function(){
+        expect(typeof sut.sequence).to.equal("function");
+      });
+    });
+
+  });
+})();
